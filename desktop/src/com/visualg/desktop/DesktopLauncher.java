61868da74d4bf8961fec1e215224d6efed21f755
azoraw <albert.zoraw@gmail.com>
2021-08-23 17:43:42 +0200

    Add Template

--- desktop/src/com/visualg/desktop/DesktopLauncher.java
+++ desktop/src/com/visualg/desktop/DesktopLauncher.java
@@ -2,13 +2,13 @@ package com.visualg.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.visualg.langtonant.LangtonAnt;
+import com.visualg.template.Template;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.setFromDisplayMode(LwjglApplicationConfiguration.getDesktopDisplayMode());
 
-        new LwjglApplication(new LangtonAnt(), config);
+        new LwjglApplication(new Template(), config);
     }
 }
