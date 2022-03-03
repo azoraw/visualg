a7fc54a7fb7bf68e8452c01ee3c5e088644c4d13
azoraw <al.gmail@gmail.com>
2022-03-03 20:05:13 +0100

    Add more settings to WigglyCircle

--- core/src/com/visualg/wigglyCircle/WigglyCircleAlg.java
+++ core/src/com/visualg/wigglyCircle/WigglyCircleAlg.java
@@ -11,10 +11,8 @@ import static java.lang.Math.sin;
 
 class WigglyCircleAlg {
 
-    private int MAX_RADIUS = 700;
     private int THRESHOLD_RADIUS = 250;
-    private static final int MIN_RADIUS_OFFSET = 300;
-    private static final int VERTEX_NUMBER = 1000; //2499 max
+
     private double NOISE_DELTA = 1;
     private final float xMiddle;
     private final float yMiddle;
@@ -31,10 +29,9 @@ class WigglyCircleAlg {
 
     List<Pair<Float, Float>> getVertices() {
         ArrayList<Pair<Float, Float>> points = new ArrayList<>();
-        for (float a = 0; a < 2 * Math.PI; a += 2 * Math.PI / VERTEX_NUMBER) {
-            double r = MIN_RADIUS_OFFSET + MAX_RADIUS * noise.eval(NOISE_DELTA * cos(a), NOISE_DELTA * sin(a), settings.getZ());
-
-            if (settings.isApplyThreshold()) {
+        for (float a = 0; a < 2 * Math.PI; a += 2 * Math.PI / settings.getVertexNumber()) {
+            double r = settings.getStartingRadius() + settings.getMaxRadius() * noise.eval(NOISE_DELTA * cos(a), NOISE_DELTA * sin(a), settings.getZ());
+            if (settings.isThresholdEnabled()) {
                 r = Math.min(r, THRESHOLD_RADIUS);
             }
             float x = (float) (xMiddle + r * cos(a));
@@ -45,7 +42,6 @@ class WigglyCircleAlg {
     }
 
     void update() {
-       // Z+= 0.01;
         settings.incrementZ();
     }
 
--- core/src/com/visualg/wigglyCircle/WigglyCircleSettings.java
+++ core/src/com/visualg/wigglyCircle/WigglyCircleSettings.java
@@ -5,8 +5,9 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Timer;
+import ui.DefaultTextField;
 
 import static com.visualg.global.Config.skin;
 import static java.lang.String.valueOf;
@@ -14,48 +15,57 @@ import static java.lang.String.valueOf;
 class WigglyCircleSettings extends Table {
 
     private final CurrentSettings currentSettings;
-    private final Label zValue;
 
     public WigglyCircleSettings(CurrentSettings currentSettings) {
         this.currentSettings = currentSettings;
         setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         top().right();
         Label empty = new Label("", skin);
-        this.add(empty);
+        add(empty);
         row();
 
         Label settings = new Label("Settings", skin);
-        this.add(settings);
+        add(settings);
         row();
 
         CheckBox threshold = new CheckBox("threshold", skin);
         threshold.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                currentSettings.setApplyThreshold(!currentSettings.isApplyThreshold());
-                zValue.setText(valueOf(currentSettings.isApplyThreshold()));
+                currentSettings.setThresholdEnabled(!currentSettings.isThresholdEnabled());
             }
         });
         add(threshold);
         row();
 
-        zValue = new Label(valueOf(currentSettings.getZ()), skin);
-        add(zValue);
-        createTimer();
+        Label maxDeltaRadius = new Label("max delta radius", skin);
+        add(maxDeltaRadius);
+        TextField maxDeltaRadiusTextField = new DefaultTextField(valueOf(currentSettings.getMaxRadius()),
+                stringValue -> currentSettings.setMaxRadius(Integer.parseInt(stringValue)));
+        add(maxDeltaRadiusTextField);
+        row();
+
+        Label vertexNumber = new Label("vertexNumber", skin);
+        add(vertexNumber);
+        TextField vertexNumberTextField = new DefaultTextField(valueOf(currentSettings.getVertexNumber()),
+                e -> currentSettings.setVertexNumber(Integer.parseInt(e)));
+
+        add(vertexNumberTextField);
+        row();
+
+        Label startingRadius = new Label("startingRadius", skin);
+        add(startingRadius);
+        TextField startingRadiusTextField = new DefaultTextField(valueOf(currentSettings.getStartingRadius()), e -> currentSettings.setStartingRadius(Integer.parseInt(e)));
+        add(startingRadiusTextField);
+        row();
+
+        Label changeSpeed = new Label("changeSpeed", skin);
+        add(changeSpeed);
+        TextField changeSpeedTextField = new DefaultTextField(valueOf(currentSettings.getZDelta()), e -> currentSettings.setZDelta(Float.parseFloat(e)));
+        add(changeSpeedTextField);
+        row();
+
     }
 
-    private void createTimer() {
-        Timer timer = new Timer();
-        timer.scheduleTask(new Timer.Task() {
-            @Override
-            public void run() {
-                update();
-            }
-        }, 0, 0.1f);
-    }
 
-
-    void update() {
-        zValue.setText(String.format("%.2f", currentSettings.getZ()));
-    }
 }
