9d1eb202df936a559ed43ee87bfa535133d9d1e3
azoraw <al.gmail@gmail.com>
2022-05-03 22:52:48 +0200

    Add 1 dimension cellular automata

--- desktop/src/com/visualg/desktop/DesktopLauncher.java
+++ desktop/src/com/visualg/desktop/DesktopLauncher.java
@@ -8,7 +8,8 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.setFromDisplayMode(LwjglApplicationConfiguration.getDesktopDisplayMode());
-
+        config.width = 1920;
+        config.height = 1080;
         new LwjglApplication(new Main(), config);
     }
 }
