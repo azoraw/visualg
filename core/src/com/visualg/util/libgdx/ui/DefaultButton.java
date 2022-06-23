411e2ab9a58ccdb8d93ee2572c784d589575d148
azoraw <albert.zoraw@gmail.com>
2022-06-23 16:00:01 +0200

    Add Runnable default button

---
+++ core/src/com/visualg/util/libgdx/ui/DefaultButton.java
@@ -0,0 +1,19 @@
+package com.visualg.util.libgdx.ui;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.visualg.global.Config;
+
+public class DefaultButton extends TextButton {
+
+    public DefaultButton(String label, Runnable onClicked) {
+        super(label, Config.skin);
+        addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                onClicked.run();
+            }
+        });
+    }
+}
