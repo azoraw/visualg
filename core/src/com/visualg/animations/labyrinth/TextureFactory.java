bb9113e3e37a9d449a98e96505fbe6a37dcc0317
azoraw <albert.zoraw@gmail.com>
2022-06-26 13:14:10 +0200

    Add labirynth generator and A star

---
+++ core/src/com/visualg/animations/labyrinth/TextureFactory.java
@@ -0,0 +1,48 @@
+package com.visualg.animations.labyrinth;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.visualg.util.Direction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.visualg.animations.labyrinth.LabyrinthGenerator.CELL_HEIGHT;
+import static com.visualg.animations.labyrinth.LabyrinthGenerator.CELL_WIDTH;
+
+public class TextureFactory {
+
+    public static Map<Direction, Texture> createWallTextures() {
+        Map<Direction, Texture> wallTextures = new HashMap<>();
+
+        wallTextures.put(Direction.LEFT, new Texture(createScaledCell(Gdx.files.internal("LEFT.png"))));
+        wallTextures.put(Direction.RIGHT, new Texture(createScaledCell(Gdx.files.internal("RIGHT.png"))));
+        wallTextures.put(Direction.DOWN, new Texture(createScaledCell(Gdx.files.internal("DOWN.png"))));
+        wallTextures.put(Direction.UP, new Texture(createScaledCell(Gdx.files.internal("UP.png"))));
+
+        return wallTextures;
+    }
+
+    public static Map<Color, Texture> createBackgroundTextures() {
+        Map<Color, Texture> backgroundTextures = new HashMap<>();
+
+        backgroundTextures.put(Color.RED, new Texture(createScaledCell(Gdx.files.internal("Red.png"))));
+        backgroundTextures.put(Color.GREEN, new Texture(createScaledCell(Gdx.files.internal("Green.png"))));
+        backgroundTextures.put(Color.BLUE, new Texture(createScaledCell(Gdx.files.internal("Blue.png"))));
+        backgroundTextures.put(Color.WHITE, new Texture(createScaledCell(Gdx.files.internal("WhiteCentered.png"))));
+
+        return backgroundTextures;
+    }
+
+    private static Pixmap createScaledCell(FileHandle file) {
+        Pixmap pixmap = new Pixmap(file);
+        Pixmap scaledPixmap = new Pixmap(CELL_WIDTH, CELL_HEIGHT, pixmap.getFormat());
+        scaledPixmap.drawPixmap(pixmap,
+                0, 0, pixmap.getWidth(), pixmap.getHeight(),
+                0, 0, scaledPixmap.getWidth(), scaledPixmap.getHeight()
+        );
+        return scaledPixmap;
+    }
+}
---
+++ core/src/com/visualg/animations/labyrinth/finder/AStarAlg.java
@@ -0,0 +1,94 @@
+package com.visualg.animations.labyrinth.finder;
+
+import com.visualg.animations.labyrinth.Cell;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.visualg.animations.labyrinth.LabyrinthGenerator.GRID_HEIGHT;
+import static com.visualg.animations.labyrinth.LabyrinthGenerator.GRID_WIDTH;
+
+
+public class AStarAlg extends PathFinder {
+
+    private static final int INIT_POSITION_X = 0;
+    private static final int INIT_POSITION_Y = 0;
+    private final Cell goal;
+
+    private Map<Cell, Integer> potentialCells = new HashMap<>();
+    private List<Cell> visitedCells = new ArrayList<>();
+
+    @Getter
+    private Cell currentCell;
+
+    public AStarAlg(Cell[][] cells) {
+        super(cells);
+        this.goal = cells[GRID_WIDTH - 1][GRID_HEIGHT - 1];
+        currentCell = cells[INIT_POSITION_X][INIT_POSITION_Y];
+    }
+
+    @Override
+    public void run() {
+        int distanceFromBeginning = 0;
+        while (isCurrentCellNotDestination()) {
+
+            visitedCells.add(currentCell);
+            potentialCells.remove(currentCell);
+            List<Cell> neighbours = getNeighbours(currentCell);
+
+            if (isNotCrossroad(currentCell)) {
+                followPath(neighbours);
+                continue;
+            }
+
+            addPotentialCells(distanceFromBeginning, neighbours);
+            goToNextCell();
+            distanceFromBeginning = potentialCells.get(currentCell);
+            sleep();
+        }
+    }
+
+    private void goToNextCell() {
+        int bestF = Integer.MAX_VALUE;
+        for (Cell potentialCell : potentialCells.keySet()) {
+            int potentialBestF = potentialCells.get(potentialCell) + calculateDistanceHeuristic(potentialCell);
+            if (potentialBestF < bestF) {
+                currentCell = potentialCell;
+                bestF = potentialBestF;
+            }
+        }
+    }
+
+    private void addPotentialCells(int g, List<Cell> neighbours) {
+        for (Cell neighbour : neighbours) {
+            if (!potentialCells.containsKey(neighbour) && !visitedCells.contains(neighbour)) {
+                potentialCells.put(neighbour, g + 1);
+            }
+        }
+    }
+
+    private boolean isCurrentCellNotDestination() {
+        return !(currentCell.getX() == goal.getX() && currentCell.getY() == goal.getY());
+    }
+
+    private boolean isNotCrossroad(Cell cell) {
+        return cell.getWalls().size() == 2;
+    }
+
+    private void followPath(List<Cell> neighbours) {
+        for (Cell neighbour : neighbours) {
+            if (!visitedCells.contains(neighbour)) {
+                currentCell = neighbour;
+            }
+        }
+        sleep();
+    }
+
+    private int calculateDistanceHeuristic(Cell cell) {
+        return goal.getX() - cell.getX() + goal.getY() - cell.getY();
+    }
+
+}
