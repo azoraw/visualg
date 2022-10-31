cee5887f722b127e9cd4d4b5d205ef1fb88d4489
azoraw <al.gmail@gmail.com>
2022-10-31 20:14:57 +0100

    Add user defined colors

--- core/src/com/visualg/util/color/PaletteType.java
+++ core/src/com/visualg/util/color/PaletteType.java
@@ -1,21 +1,22 @@
 package com.visualg.util.color;
 
 import com.badlogic.gdx.graphics.Color;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import static com.badlogic.gdx.graphics.Color.BLACK;
 import static com.badlogic.gdx.graphics.Color.WHITE;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
 public enum PaletteType {
     DARK(BLACK, WHITE),
-    BRIGHT(WHITE, BLACK);
-    //DEFINED_BY_USER(null);
+    BRIGHT(WHITE, BLACK),
+    USER_DEFINED(BLACK, WHITE);
 
-    @Getter
-    private final Color background;
-    @Getter
-    private final Color primaryColor;
+    @Getter @Setter
+    private Color background;
+    @Getter @Setter
+    private Color primaryColor;
 
 }
