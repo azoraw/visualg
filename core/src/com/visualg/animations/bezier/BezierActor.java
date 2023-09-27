514ed2b6a70aac8d6cf45b8d53fb68dc28f268ff
azoraw <albert.zoraw@gmail.com>
2023-09-27 21:01:00 +0200

    Add random colors to BezierActor

--- core/src/com/visualg/animations/bezier/BezierActor.java
+++ core/src/com/visualg/animations/bezier/BezierActor.java
@@ -16,7 +16,7 @@ public class BezierActor extends ShapeRendererActor {
     @Override
     protected void drawFrame() {
         for (Line line : oldWidowsScreenSaver.getLines()) {
-            sr.line(line.end1(), line.end2());
+            sr.line(line.end1().x, line.end1().y, line.end2().x, line.end2().y, line.colorPair().first(), line.colorPair().second());
         }
         oldWidowsScreenSaver.update();
     }
